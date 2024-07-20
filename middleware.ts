b1edb86724aaf9e41d2214915d7254b3d29695a8
maahin2005 <mahin.malek2005@gmail.com>
2024-07-20 23:19:16 +0530

    Fixing:{BUG}

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default authMiddleware({});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
