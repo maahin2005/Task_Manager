458aeee5869feef8113645c594e5c4f6d2accc84
maahin2005 <mahin.malek2005@gmail.com>
2024-07-20 21:45:50 +0530

    POST_Route( ):Creating Task Controller

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,9 +1,31 @@
+import prisma from "@/app/utils/connect";
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     try {
         
+     const {userId} = auth();
 
+     if(!userId) {
+        return NextResponse.json({error: "Unauthorized", status: 401});
+     }
+
+     const {title,description,date,completed,important} = await req.json();
+
+     if(!title || !description || !date ){
+         return NextResponse.json({error: "Missing required Fields",status: 400})
+     }
+
+     if(title.length < 3){
+        return NextResponse.json({error: "Title must be at least 3 characters Long", status: 400})
+     }
+
+     const task = await prisma.task.create({
+        data:{title, description, date, isCompleted:completed, isImportant:important, userId}
+     })
+
+     return NextResponse.json({task,msg:"Task Added Successfully"})
 
     } catch (error) {
         console.error("ERROR CREATING TASK: ", (error as Error).message, error);
