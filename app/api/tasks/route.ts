47aeea2674996cad97f794d560b011882de2dc70
maahin2005 <mahin.malek2005@gmail.com>
2024-07-20 20:49:31 +0530

    initializing Backend Routes

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -2,9 +2,48 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     try {
-        // Your code that might throw an error
+        
+
+
     } catch (error) {
         console.error("ERROR CREATING TASK: ", (error as Error).message, error);
+
+        return NextResponse.json({ error: "Error creating task" }, { status: 500 });
+    }
+}
+
+export async function GET(req: Request) {
+    try {
+        
+        
+
+    } catch (error) {
+        console.error("ERROR GETTING TASK: ", (error as Error).message, error);
+
+        return NextResponse.json({ error: "Error creating task" }, { status: 500 });
+    }
+}
+
+export async function PUT(req: Request) {
+    try {
+        
+        
+
+    } catch (error) {
+        console.error("ERROR UPDATING TASK: ", (error as Error).message, error);
+
+        return NextResponse.json({ error: "Error creating task" }, { status: 500 });
+    }
+}
+
+
+export async function DELETE(req: Request) {
+    try {
+        
+        
+
+    } catch (error) {
+        console.error("ERROR DELETING TASK: ", (error as Error).message, error);
 
         return NextResponse.json({ error: "Error creating task" }, { status: 500 });
     }
