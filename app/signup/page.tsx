7d32f5590c2e26c549d56ed587eeff0f7d24bfb9
maahin2005 <mahin.malek2005@gmail.com>
2024-07-21 19:57:42 +0530

    Authentication Pages

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-
 import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
 function page() {
   return (
-    <div>
+    <div className="flex items-center justify-center h-full">
       <SignUp />
     </div>
   );
