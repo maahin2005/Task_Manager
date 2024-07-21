f378d87ee1282cfb2bae0ebb3fe2175d138c770b
maahin2005 <mahin.malek2005@gmail.com>
2024-07-21 20:00:18 +0530

    Updating Contexts

--- app/providers/ContextProvider.tsx
+++ app/providers/ContextProvider.tsx
@@ -1,26 +1,35 @@
 "use client";
-
-import React, { useEffect, useState } from "react";
-import GlobalProvider from "../context/globalProvider";
+import React from "react";
+import { GlobalProvider } from "../context/globalProvider";
+import { Toaster } from "react-hot-toast";
 
 interface Props {
   children: React.ReactNode;
 }
 
 function ContextProvider({ children }: Props) {
-  const [isReady, setIsReady] = useState(false);
+  const [isReady, setIsReady] = React.useState(false);
 
-  useEffect(() => {
+  React.useEffect(() => {
     setTimeout(() => {
       setIsReady(true);
-    }, 200);
+    }, 250);
   }, []);
 
   if (!isReady) {
-    return null;
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <span className="loader"></span>
+      </div>
+    );
   }
 
-  return <GlobalProvider>{children}</GlobalProvider>;
+  return (
+    <GlobalProvider>
+      <Toaster />
+      {children}
+    </GlobalProvider>
+  );
 }
 
 export default ContextProvider;
