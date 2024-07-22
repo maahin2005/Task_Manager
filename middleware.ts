5d46217dc8af5473b622c4bf587be4321fc33930
maahin2005 <mahin.malek2005@gmail.com>
2024-07-22 19:46:18 +0530

    Updating Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
-
 export default authMiddleware({});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/"],
+  publicRoutes: ["/"],
 };
